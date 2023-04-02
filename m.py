1903b165bed85e04991e1c6f9d9fc17a04cf0926
lyphrowny <strukov.as@edu.spbstu.ru>
2023-04-02 16:55:03 +0300

    Add test python file

---
+++ m.py
@@ -0,0 +1,11 @@
+import numpy
+
+import n
+
+from numpy import int8
+
+def MySt_yle():
+        thisissomeverylongline_thisissomeverylongline_thisissomeverylongline_thisissomeverylongline_thisissomeverylongline_thisissomeverylongline_thisissomeverylongline_ = ""
+        
+    myProg = "a"
+    return 0.
