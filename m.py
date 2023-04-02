104b24db7f12451a719b778f9a1ef142c7e09f77
lyphrowny <strukov.as@edu.spbstu.ru>
2023-04-02 17:03:13 +0300

    Fix indent

--- m.py
+++ m.py
@@ -5,7 +5,7 @@ import n
 from numpy import int8
 
 def MySt_yle():
-        thisissomeverylongline_thisissomeverylongline_thisissomeverylongline_thisissomeverylongline_thisissomeverylongline_thisissomeverylongline_thisissomeverylongline_ = ""
+    thisissomeverylongline_thisissomeverylongline_thisissomeverylongline_thisissomeverylongline_thisissomeverylongline_thisissomeverylongline_thisissomeverylongline_ = ""
         
     myProg = "a"
     return 0.
